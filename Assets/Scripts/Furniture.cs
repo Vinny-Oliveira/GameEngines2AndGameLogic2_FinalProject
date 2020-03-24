8ac7e01274896d016bbd00a95a161d3bb57bca23
Vinicius Oliveira <vr.sena@gmail.com>
2020-03-23 22:13:48 -0400

    Furniture tests

---
+++ Assets/Scripts/Furniture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : MonoBehaviour {
+
+    [SerializeField]
+    int health;
+
+    // Constructors
+    Furniture() {
+        health = 0;
+    }
+
+    Furniture(int new_health) {
+        health = new_health;
+    }
+
+    // Member methods
+
+    /// <summary>
+    /// Decrease health of the object
+    /// </summary>
+    public void DecreaseHealth() {
+        health--;
+
+        if (health < 1) {
+            gameObject.SetActive(false);
+        }
+    }
+}
