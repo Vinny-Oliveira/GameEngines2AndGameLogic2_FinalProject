e9f419682d1b9afd481ba974123f1090e62d138b
Vinicius Oliveira <vr.sena@gmail.com>
2020-03-24 22:28:06 -0400

    Started hammer object and coin manager

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour {
+
+    int intCoins;
+
+    const int START_COINS = 0;
+
+    private void Start() {
+        intCoins = START_COINS;
+    }
+
+    /// <summary>
+    /// Add or remove coins to the total coin count
+    /// </summary>
+    /// <param name="movedCoins"></param>
+    public void CoinManipulator(int movedCoins) {
+        intCoins += movedCoins;
+    }
+
+
+}
---
+++ Assets/Scripts/Hammer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer {
+
+    int intHealth;
+
+    const int HAMMER_MAX_HEALTH = 10;
+
+    /// <summary>
+    /// No-args Constructor of the Hammer
+    /// </summary>
+    Hammer() {
+        intHealth = HAMMER_MAX_HEALTH;
+    }
+
+    /// <summary>
+    /// Getter of the hammer health
+    /// </summary>
+    /// <returns></returns>
+    public int GetHammerHealth() {
+        return intHealth;
+    }
+
+    /// <summary>
+    /// Decrease the health of the hammer
+    /// </summary>
+    public void DecreaseHealth() {
+        intHealth--;
+    }
+}
