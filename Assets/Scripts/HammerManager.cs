cf5a541fe204e28d0d6e5cab4ab1be39e842414a
Vinicius Oliveira <vr.sena@gmail.com>
2020-03-25 11:20:44 -0400

    Tests with hammer manager

--- Assets/Scripts/HammerManager.cs
+++ Assets/Scripts/HammerManager.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class HammerManager : MonoBehaviour {
 
     Queue<Hammer> queHammers = new Queue<Hammer>();
     public int intStartingHammers;
-    public TextMeshProUGUI tmpHammerCount;
+    //public TextMeshProUGUI tmpHammerCount;
+    public Text tmpHammerCount;
+    public GameObject pnl_GameOver;
+
+    public static bool canHit;
+    public static HammerManager instance;
+
+    private void Awake() {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start() {
+        canHit = true;
         PopulateHammerQueue(intStartingHammers);
+        tmpHammerCount.text = queHammers.Count.ToString();
     }
 
     /// <summary>
@@ -21,6 +33,22 @@ public class HammerManager : MonoBehaviour {
     public void PopulateHammerQueue(int amountHammers) { 
         for (int i = 0; i < amountHammers; i++) {
             queHammers.Enqueue(new Hammer());
+        }
+    }
+
+    /// <summary>
+    /// The Hammer in the beginning of the queue hits an object and loses health
+    /// </summary>
+    public void HitAnObject() {
+        queHammers.Peek().DecreaseHealth();
+        
+        if (queHammers.Peek().IsHammerBroken()) {
+            queHammers.Dequeue();
+            tmpHammerCount.text = queHammers.Count.ToString();
+
+            if (queHammers.Count < 1) {
+                pnl_GameOver.SetActive(true);
+            }
         }
     }
 }
