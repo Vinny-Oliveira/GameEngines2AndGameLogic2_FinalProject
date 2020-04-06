b65a17698cc5e58e4a26e9aaa00690c93d276fe4
Amanda <amandaduarte2012@gmail.com>
2020-04-06 20:14:30 -0300

    Added VolumeControl script
    + Change Settings Menu in the Main Menu
    + Sync Audio Mixers with Sliders
    + Added Soundtrack to the Main Menu scene

---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetMasterVol(float volMaster)
+    {
+        masterMixer.SetFloat("volMaster", volMaster);
+    }
+
+    public void SetSfxVol(float volSFXs)
+    {
+        masterMixer.SetFloat("volSFXs", volSFXs);
+    }
+
+    public void SetMusicVol(float volMusic)
+    {
+        masterMixer.SetFloat("volMusic", volMusic);
+    }
+
+    public void SetMenuSoundsVol(float volMenuSounds)
+    {
+        masterMixer.SetFloat("volMenuSounds", volMenuSounds);
+    }
+}
