a25e7f88abe5a8a040b10a95872d591fea22fda5
Vinicius Oliveira <vr.sena@gmail.com>
2020-04-09 13:17:12 -0400

    Started the prototype for the in game purchases

---
+++ Assets/Scripts/InGameProductDisplayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InGameProductDisplayer : MonoBehaviour {
+
+    [Header("Product Details")]
+    public Image imgProduct;
+    public int intPrice;
+    public TextMeshProUGUI tmpPrice;
+
+    [Header("UI Control")]
+    public TextMeshProUGUI tmpAmountToBuy;
+    public Button btnMinus;
+    public Button btnPlus;
+}
