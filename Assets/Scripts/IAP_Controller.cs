5ceb76b0975828576b9f3156ced1b02c9b3ec7f1
Vinicius Oliveira <vr.sena@gmail.com>
2020-04-11 17:53:26 -0400

    Added a description to the IAP_Controller

--- Assets/Scripts/IAP_Controller.cs
+++ Assets/Scripts/IAP_Controller.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// Control the In-App Purchases of the game
+/// </summary>
 public class IAP_Controller : MonoBehaviour {
 
     [Header("UI References")]
